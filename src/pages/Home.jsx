4c293e225b162c2eab226088ee94cec1889ffbb3
Jarda <jardadjarda@gmail.com>
2026-09-26 19:20:12 +0200

    fix getCategories icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,14 @@
 import "../css/home.css";
-import { useState, useEffect } from "react";
 import Cardcategories from "../components/Cardcategories";
+import { useBazosContext } from "../context/BazosContext";
 
 function Home() {
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    const getCategories = async () => {
-      try {
-        const resCategories = await fetch(
-          "https://bazos-yihu.onrender.com/api/categories"
-        );
-
-        if (!resCategories.ok) {
-          throw new Error({ message: "Ошибка при загрузке категории" });
-        }
-
-        const dataCategories = await resCategories.json();
-
-        setCategories(
-          dataCategories.map((category) => {
-            if (category.slug === "auto") {
-              return { ...category, icon: "./images/auto.svg" };
-            } else if (category.slug === "elektro") {
-              return { ...category, icon: "./images/elektro.svg" };
-            } else if (category.slug === "mobily") {
-              return { ...category, icon: "./images/mobil.svg" };
-            } else if (category.slug === "pc") {
-              return { ...category, icon: "./images/pc.svg" };
-            }
-
-            return category;
-          })
-        );
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    getCategories();
-
-    return () => getCategories();
-  }, []);
+  const { subCategories } = useBazosContext();
 
   return (
     <div className="wrapper-home">
       <div className="wrapper-grids">
-        {categories.map((category) => (
+        {subCategories.map((category) => (
           <Cardcategories key={category.id} category={category} />
         ))}
       </div>
