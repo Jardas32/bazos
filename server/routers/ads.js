05a05aed7d34dd1f16ddf1d19e9c0d3bec20906e
Jarda <jardadjarda@gmail.com>
2026-09-26 22:13:46 +0200

    fix import auth

--- server/routers/ads.js
+++ server/routers/ads.js
@@ -4,7 +4,7 @@ import multer from "multer";
 import path from "path";
 import { db } from "../db.js";
 import upload from "../middleware/upload.js";
-import { auth } from "../middleware/auth.js";
+import auth from "../middleware/auth.js";
 import { v2 as cloudinary } from "cloudinary";
 
 const router = express.Router();
